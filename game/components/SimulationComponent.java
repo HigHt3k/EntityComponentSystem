692f76104504913884eba81162f1424e0f3f5d0d
Johann <johanntoepfer@gmail.com>
2023-01-03 11:48:26 +0100

    added a group id to determine components located inside a group

--- game/components/SimulationComponent.java
+++ game/components/SimulationComponent.java
@@ -1,10 +1,12 @@
 package game.components;
 
+import com.IdGenerator;
 import com.ecs.component.Component;
 
 public class SimulationComponent extends Component {
     private float failureRatio;
     private SimulationType simulationType;
+    private int groupId;
 
     @Override
     public void update() {
@@ -25,5 +27,15 @@ public class SimulationComponent extends Component {
 
     public void setSimulationType(SimulationType simulationType) {
         this.simulationType = simulationType;
+        if(simulationType == SimulationType.CPU)
+            groupId = IdGenerator.generateId();
+    }
+
+    public void setGroupId() {
+        this.groupId = groupId;
+    }
+
+    public int getGroupId() {
+        return groupId;
     }
 }
