f54ac0808a25e39ffdd8a5a29901173b46060380
Johann <johanntoepfer@gmail.com>
2023-01-11 14:58:17 +0100

    variable name updated

--- game/components/CableColors.java
+++ game/components/CableColors.java
@@ -6,5 +6,5 @@ public class CableColors {
     public static Color PORT_1_COLOR = new Color(255, 0, 0);
     public static Color PORT_2_COLOR = new Color(0, 255, 0);
     public static Color PORT_3_COLOR = new Color(0, 0, 255);
-    public static Color Port_4_COLOR = new Color(255, 255, 0);
+    public static Color PORT_4_COLOR = new Color(255, 255, 0);
 }
