001bb6096c635d55d4ff12989625beb5e17a7a19
Johann <johanntoepfer@gmail.com>
2023-01-12 09:18:49 +0100

    shapes with 0 size are null objects and lead to bugs; shapes that are null are not being rendered now

--- com/graphics/render/ShapeRenderer.java
+++ com/graphics/render/ShapeRenderer.java
@@ -5,6 +5,9 @@ import java.awt.*;
 public class ShapeRenderer {
 
     public static void render(Graphics2D g, Shape shape, Color borderColor, Color fillColor) {
+        if(shape == null) {
+            return;
+        }
         if(fillColor != null) {
             g.setColor(fillColor);
             g.fill(shape);
