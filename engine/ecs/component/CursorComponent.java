3ae0041bf42291d9c4f956d1dfd81cd8be247e2b
Johann <johanntoepfer@gmail.com>
2023-02-15 09:38:10 +0100

    implemented: selected entity stored in component; cursor position now scales according to screen size

--- engine/ecs/component/CursorComponent.java
+++ engine/ecs/component/CursorComponent.java
@@ -1,13 +1,25 @@
 package engine.ecs.component;
 
+import engine.Game;
+import engine.ecs.entity.Entity;
+
 import java.awt.*;
 
 public class CursorComponent extends Component {
     private Point cursorPosition;
+    private Entity selected;
 
     @Override
     public void update() {
 
+    }
+
+    public Entity getSelected() {
+        return selected;
+    }
+
+    public void setSelected(Entity selected) {
+        this.selected = selected;
     }
 
     public Point getCursorPosition() {
@@ -15,6 +27,6 @@ public class CursorComponent extends Component {
     }
 
     public void setCursorPosition(Point cursorPosition) {
-        this.cursorPosition = cursorPosition;
+        this.cursorPosition = Game.scale().scalePoint(cursorPosition);
     }
 }
