087002fccf421a1142fa7a90db96e80a65940e1c
Johann <johanntoepfer@gmail.com>
2023-02-15 11:58:06 +0100

    javadoc

--- engine/IdGenerator.java
+++ engine/IdGenerator.java
@@ -1,9 +1,16 @@
 package engine;
 
+/**
+ * Generate IDs for entities
+ */
 public class IdGenerator {
 
     private static int id = 0;
 
+    /**
+     * Generate the next available integer id
+     * @return the id
+     */
     public static int generateId() {
         id += 1;
         return id;
