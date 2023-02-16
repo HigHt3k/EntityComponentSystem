e1a8c2fefe832dda477076eb006cada4477170a0
Johann <johanntoepfer@gmail.com>
2023-02-16 12:51:51 +0100

    implemented: querying of existing list

--- engine/ecs/Query.java
+++ engine/ecs/Query.java
@@ -27,16 +27,30 @@ public class Query {
         return entities;
     }
 
+    public static <T extends Component> ArrayList<Entity> getEntitiesWithComponent(Class<T> componentClass, ArrayList<Entity> filter) {
+        ArrayList<Entity> entities = new ArrayList<>();
+        if (Game.scene().current() == null) {
+            return entities;
+        }
+        for (Entity e : filter) {
+            if (e.getComponent(componentClass) != null) {
+                entities.add(e);
+            }
+        }
+        return entities;
+    }
+
     /**
      * Query all available entities in the current scene for entities as an instance of the input class
+     *
      * @param entityClass: class of the entities to query for
      * @param <T>
      * @return List with entities that were queried
      */
     public static <T extends Entity> ArrayList<Entity> getEntitiesOfClassType(Class<T> entityClass) {
         ArrayList<Entity> entities = new ArrayList<>();
-        for(Entity e : Game.scene().current().getEntities()) {
-            if(e.getClass() == entityClass) {
+        for (Entity e : Game.scene().current().getEntities()) {
+            if (e.getClass() == entityClass) {
                 entities.add(e);
             }
         }
