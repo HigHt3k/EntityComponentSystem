8880e841622a7bd808f00d40694a00bfefe10e99
Johann <johanntoepfer@gmail.com>
2023-02-21 14:38:07 +0100

    implemented: keyboard button click

--- game/handler/CursorSelectorHandler.java
+++ game/handler/CursorSelectorHandler.java
@@ -35,29 +35,43 @@ public class CursorSelectorHandler extends Handler {
 
     @Override
     public void handle(KeyEvent e) {
-        ArrayList<Entity> collisionEntities = Query.getEntitiesWithComponent(ColliderComponent.class);
         int x = cursor.getComponent(RenderComponent.class).getRenderObjects().get(0).getBounds().getBounds().x;
         int y = cursor.getComponent(RenderComponent.class).getRenderObjects().get(0).getBounds().getBounds().y;
 
         switch (e.getKeyCode()) {
             case KeyEvent.VK_RIGHT -> {
                 x += 1 * Game.config().getControls().getCursorSpeed();
+                moveCursor(x, y);
             }
             case KeyEvent.VK_LEFT -> {
                 x -= 1 * Game.config().getControls().getCursorSpeed();
+                moveCursor(x, y);
             }
             case KeyEvent.VK_DOWN -> {
                 y += 1 * Game.config().getControls().getCursorSpeed();
+                moveCursor(x, y);
             }
             case KeyEvent.VK_UP -> {
                 y -= 1 * Game.config().getControls().getCursorSpeed();
+                moveCursor(x, y);
+            }
+            case KeyEvent.VK_ENTER -> {
+                select();
             }
         }
+    }
 
+    @Override
+    public void handle(MouseEvent e) {
+
+    }
+
+    private void moveCursor(int x, int y) {
+        ArrayList<Entity> collisionEntities = Query.getEntitiesWithComponent(ColliderComponent.class);
         cursor.getComponent(RenderComponent.class).reposition(new Point(x, y));
         cursor.getComponent(CursorComponent.class).reposition(new Point(x, y));
 
-        for(Entity entity : collisionEntities) {
+        for (Entity entity : collisionEntities) {
             for (CollisionObject c : entity.getComponent(ColliderComponent.class).getCollisionObjects()) {
                 if (c.getCollisionBoundaries()
                         .contains(cursor.getComponent(CursorComponent.class).getCursorPosition())) {
@@ -72,8 +86,13 @@ public class CursorSelectorHandler extends Handler {
         }
     }
 
-    @Override
-    public void handle(MouseEvent e) {
-
+    private void select() {
+        ArrayList<CollisionObject> collisionObjects = cursor.getComponent(CursorComponent.class)
+                .getSelected().getComponent(ColliderComponent.class).getCollisionObjects();
+        for (CollisionObject c : collisionObjects) {
+            if (c.isHovered()) {
+                c.setClicked(true);
+            }
+        }
     }
 }
