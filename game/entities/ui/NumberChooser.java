f471d3c49bbdf28129b4249e9b96ad1adc4b43f9
Johann <johanntoepfer@gmail.com>
2023-02-23 10:58:49 +0100

    implemented: use image instead of text

--- game/entities/ui/NumberChooser.java
+++ game/entities/ui/NumberChooser.java
@@ -4,10 +4,7 @@ import engine.ecs.component.action.ActionComponent;
 import engine.ecs.component.collision.ColliderComponent;
 import engine.ecs.component.collision.CollisionObject;
 import engine.ecs.component.graphics.RenderComponent;
-import engine.ecs.component.graphics.objects.HoverObject;
-import engine.ecs.component.graphics.objects.Layer;
-import engine.ecs.component.graphics.objects.ShapeObject;
-import engine.ecs.component.graphics.objects.TextObject;
+import engine.ecs.component.graphics.objects.*;
 import engine.ecs.entity.Entity;
 import engine.graphics.render.TextHorizontalAlignment;
 import engine.graphics.render.TextVerticalAlignment;
@@ -39,6 +36,34 @@ public class NumberChooser extends Entity {
         renderComponent.addRenderObject(new TextObject(new Point(x, y),
                 new Rectangle(x, y, width, height), Layer.UI, text, FontCollection.bit8Font, Bit8.DARK_GREY,
                 TextHorizontalAlignment.CENTER, TextVerticalAlignment.CENTER));
+
+        HoverObject hover = new HoverObject(new Point(x, y), new Rectangle(x, y, width, height), HOVER_COLOR, Layer.UI_HOVER);
+        renderComponent.addRenderObject(hover);
+
+        ColliderComponent colliderComponent = new ColliderComponent();
+        colliderComponent.setEntity(this);
+        this.addComponent(colliderComponent);
+        colliderComponent.addCollisionObject(new CollisionObject(new Rectangle(x, y, width, height), hover));
+
+        ActionComponent actionComponent = new ActionComponent();
+        actionComponent.setEntity(this);
+        this.addComponent(actionComponent);
+        actionComponent.addAction(MouseEvent.BUTTON1, new SetAmountAction(c, amount));
+    }
+
+    public NumberChooser(String name, int id, BufferedImage img, int x, int y, int width,
+                         int height, int amount, BuildPanelEntity c) {
+        super(name, id);
+
+        RenderComponent renderComponent = new RenderComponent();
+        renderComponent.setEntity(this);
+        this.addComponent(renderComponent);
+
+        ShapeObject shapeObject = new ShapeObject(new Point(x, y),
+                new Rectangle(x, y, width, height), Layer.UI, Bit8.TRANSPARENT, Bit8.TRANSPARENT, 1);
+        renderComponent.addRenderObject(shapeObject);
+
+        renderComponent.addRenderObject(new ImageObject(new Point(x, y), new Rectangle(x, y, width, height), Layer.UI, img));
 
         HoverObject hover = new HoverObject(new Point(x, y), new Rectangle(x, y, width, height), HOVER_COLOR, Layer.UI_HOVER);
         renderComponent.addRenderObject(hover);
