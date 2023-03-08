38b50c44f99d4b071e2b65fd8977991b530c9606
hight3k <johanntoepfer@gmail.com>
2023-03-08 18:00:58 +0100

    implemented: placeholder for testing methods of markov processor

---
+++ src/test/java/game/handler/simulation/markov/MarkovProcessorTest.java
@@ -0,0 +1,12 @@
+package game.handler.simulation.markov;
+
+
+import org.junit.jupiter.api.Test;
+
+class MarkovProcessorTest {
+
+    @Test
+    void markovStart() {
+        //TODO: Implement testing methods
+    }
+}
