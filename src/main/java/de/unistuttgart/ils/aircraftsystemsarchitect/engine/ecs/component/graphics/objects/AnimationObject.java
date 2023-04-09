a72277e9cd449aa9bf789f1ec1acf5f96f0c8fe0
hight3k <johanntoepfer@gmail.com>
2023-04-09 13:58:42 +0200

    javadoc

--- src/main/java/de/unistuttgart/ils/aircraftsystemsarchitect/engine/ecs/component/graphics/objects/AnimationObject.java
+++ src/main/java/de/unistuttgart/ils/aircraftsystemsarchitect/engine/ecs/component/graphics/objects/AnimationObject.java
@@ -2,6 +2,10 @@ package de.unistuttgart.ils.aircraftsystemsarchitect.engine.ecs.component.graphi
 
 import java.awt.*;
 
+/**
+ * AnimationObjects store parameters to render animations by using GIF files
+ * Contains current frame, frames that should be paused during animation and the state of the animation
+ */
 public class AnimationObject extends RenderObject {
     private final Image animation;
     private long curFrame = 0;
@@ -9,11 +13,27 @@ public class AnimationObject extends RenderObject {
     private long pauseFrames = 0;
     private boolean paused = false;
 
+    /**
+     * Create new AnimationObject
+     * @param location: location to render the object to the screen
+     * @param bounds: boundaries of the object
+     * @param layer: rendering layer
+     * @param animation: animation image (GIF File)
+     */
     public AnimationObject(Point location, Shape bounds, Layer layer, Image animation) {
         super(location, bounds, layer);
         this.animation = animation;
     }
 
+    /**
+     *
+     * @param location: location to render the object to the screen
+     * @param bounds: boundaries of the object
+     * @param layer: rendering layer
+     * @param animation: animation image (GIF File)
+     * @param randomize: randomize the animation rendering (pause between two cycles)
+     * @param pauseFrames: amount of frames that should be paused
+     */
     public AnimationObject(Point location, Shape bounds, Layer layer, Image animation, boolean randomize, long pauseFrames) {
         super(location, bounds, layer);
         this.animation = animation;
@@ -21,30 +41,58 @@ public class AnimationObject extends RenderObject {
         this.pauseFrames = pauseFrames;
     }
 
+    /**
+     * check if the animation is currently in paused state
+     * @return true of paused
+     */
     public boolean isPaused() {
         return paused;
     }
 
+    /**
+     * Set the animation state
+     * @param paused: true if paused
+     */
     public void setPaused(boolean paused) {
         this.paused = paused;
     }
 
+    /**
+     * Get the Animation
+     * @return the animation image
+     */
     public Image getAnimation() {
         return animation;
     }
 
+    /**
+     * Get the current frame of the animation
+     * @return the frame
+     */
     public long getCurFrame() {
         return curFrame;
     }
 
+    /**
+     * Set the current frame of the animation
+     * @param curFrame: the frame (integer)
+     */
     public void setCurFrame(long curFrame) {
         this.curFrame = curFrame;
     }
 
+    /**
+     * Get the frames that should be paused in between rendering the animation
+     * @return the amount of paused frames
+     */
     public long getPauseFrames() {
         return pauseFrames;
     }
 
+    /**
+     * Check if the animation should play at randomized start points
+     * @return true if randomized is on
+     */
     public boolean isRandomize() {
         return randomize;
     }
--- src/main/java/de/unistuttgart/ils/aircraftsystemsarchitect/engine/ecs/component/graphics/objects/HoverObject.java
+++ src/main/java/de/unistuttgart/ils/aircraftsystemsarchitect/engine/ecs/component/graphics/objects/HoverObject.java
@@ -2,21 +2,41 @@ package de.unistuttgart.ils.aircraftsystemsarchitect.engine.ecs.component.graphi
 
 import java.awt.*;
 
+/**
+ * HoverObjects can be used to display a transparent / non-transparent shape at a boundary at the hovering layer
+ */
 public class HoverObject extends RenderObject {
     private final Color hoverColor;
 
+    /**
+     * Create new HoverObject at given location and size, with color
+     * @param location: the location to place the object
+     * @param bounds: the boundaries of the object
+     * @param hoverColor: the color (alpha needs to be set before)
+     */
     public HoverObject(Point location, Shape bounds, Color hoverColor) {
         super(location, bounds, Layer.HOVER);
         this.hoverColor = hoverColor;
         setHidden(true);
     }
 
+    /**
+     * Create new HoverObject at given location and size, with color and at custom rendering layer
+     * @param location: the location to place the object
+     * @param bounds: the boundaries of the object
+     * @param hoverColor: the color (alpha needs to be set before)
+     * @param layer: rendering layer
+     */
     public HoverObject(Point location, Shape bounds, Color hoverColor, Layer layer) {
         super(location, bounds, layer);
         this.hoverColor = hoverColor;
         setHidden(true);
     }
 
+    /**
+     * Get the color of the hover object
+     * @return hover color
+     */
     public Color getHoverColor() {
         return hoverColor;
     }
